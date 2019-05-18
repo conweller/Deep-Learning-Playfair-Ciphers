aaa12d3242a2539e87feca656f46953f78e196b8
Chris Outhwaite <chrisout@udel.edu>
2019-05-18 13:58:30 -0400

    Added key_to_string function

--- src/cipher.py
+++ src/cipher.py
@@ -108,3 +108,8 @@ def decipher_text(key, cipher_text):
                 idx2 % 5 + (idx1 % 5 - idx2 % 5) + (idx2 // 5 * 5)
             ]
     return deciphered_text
+   
+
+def key_to_string(key):
+    for idx in range(0, 25, 5):
+        print(key[idx:idx + 5])
